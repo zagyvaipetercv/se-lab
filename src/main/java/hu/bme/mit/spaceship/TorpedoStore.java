36bf708e2b4ffb5779faa2596078837114f0b602
Peter Zagyvai <p.zagyvai@gmail.com>
2024-10-14 14:27:02 +0200

    bug fixed in TorpedoStore: generator (Random) object is now initialized outside of the fire() finction

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,9 +14,12 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
+  private Random generator;
+
   public TorpedoStore(int numberOfTorpedos) {
     this.torpedoCount = numberOfTorpedos;
 
+    this.generator = new Random();
     // update failure rate if it was specified in an environment variable
     String failureEnv = System.getenv("IVT_RATE");
     if (failureEnv != null) {
@@ -36,7 +39,6 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
